81f69d53d549d0a6023beb68ce68b6998ea5b298
Manami Ueda <manamiueda1@gmail.com>
2021-04-09 16:03:35 -0400

    restructuring

---
+++ queries.js
@@ -0,0 +1,38 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "music",
+  password: "",
+  port: 5432,
+});
+
+const getSongs = (request, response) => {
+  pool.query("SELECT * FROM songs", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    console.log(response);
+    response.status(200).json(results.rows);
+  });
+};
+
+const addSongs = (request, response) => {
+  const { song_name, downloads } = request.body;
+  pool.query(
+    `INSERT INTO songs (song_name, downloads) VALUES ($1, $2)`,
+    [song_name, downloads],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
+
+module.exports = {
+  getSongs,
+  addSongs,
+};
